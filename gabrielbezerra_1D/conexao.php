b88ad4dba6da6e6cf27b7fff3166a715d0f024c7
Gabriel Fernandes Bezerra <gabriel.fernandes.bezerra@escola.pr.gov.br>
2025-11-18 12:26:00 -0300

    Add Cloudinary configuration to conexao.php
    Added Cloudinary credentials for image management.

--- gabrielbezerra_1D/conexao.php
+++ gabrielbezerra_1D/conexao.php
@@ -15,4 +15,11 @@ if (!$conexao) {
 
 // SENSITIVE CASE suportar acentos e Ç
 mysqli_set_charset($conexao, "utf8");
+
+
+// Substituam os valores abaixo pelas credenciais da sua própria conta do Cloudinary
+$cloud_name = "dtzsyeaze";  // exemplo: "meucloud123"
+$api_key    = "844152766979421";     // exemplo: "123456789012345"
+$api_secret = "sYGM28tyZKDCyApAqU9ABtkGoe8";  // exemplo: "abcdeFGHijkLMNopqrstu"
+
 ?>
